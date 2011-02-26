01042883873a2ca6ed7474aa012b5f2619ac791a
Nanda Kishore <madhav.bnk@gmail.com>
2011-02-26 09:28:58 +0530

    Fixing 'anonymoususer' decorator issue. Placing Join/Login links on the top of static pages

--- users/decorators.py
+++ users/decorators.py
@@ -7,7 +7,7 @@ def anonymoususer(the_function):
     def _anonymoususer(request, *args, **kwargs):
         user = request.user
         if user.is_authenticated():
-            return HttpResponseRedirect(redirect_to=url_reverse('quest.views.view_homepage'))
+            return HttpResponseRedirect(redirect_to=url_reverse('users.views.view_homepage'))
         kwargs['next'] = get_data(request).get('next', '')
         return the_function(request, *args, **kwargs)
     return _anonymoususer
