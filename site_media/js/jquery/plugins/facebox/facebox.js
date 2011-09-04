b20fb10a0bfc5663814dbc9023f0bfc2f1d9b284
Nanda Kishore <madhav.bnk@gmail.com>
2011-09-04 14:58:42 +0530

    closelabel url typo. fixed

--- site_media/js/jquery/plugins/facebox/facebox.js
+++ site_media/js/jquery/plugins/facebox/facebox.js
@@ -93,7 +93,7 @@
       <div class="popup"> \
         <div class="content"> \
         </div> \
-        <a href="#" class="close"><img src="/facebox/closelabel.png" title="close" class="close_image" /></a> \
+        <a href="#" class="close"><img src="/site_media/js/jquery/plugins/facebox/closelabel.png" title="close" class="close_image" /></a> \
       </div> \
     </div>'
     },
